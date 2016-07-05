e03b482880c1dc276f572d4260f97114a71ea5e3
lorenzo892 <lorenzo.gimeno.ruiz@gmail.com>
2016-07-05 09:20:09 +0200

    Day 1

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,8 +1,25 @@
 class SiteController < ApplicationController
-  def home
-  
-  end
-  def controler
 
+  def say_name
+    @name= params[:name]
+    render 'say_name'
+  end
+
+  def home
+  end
+
+  def controler
+    render 'contact', layout: 'admin'
+  end
+
+  def calculator
+    render 'calculator'
+  end
+
+  def calculate
+    params[:first_num]
+    params[:second_num]
+    @result = params[:first_num].to_i + params[:second_num].to_i
+    render 'calculate'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'site#home'
   get '/contact', to: 'site#contact'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/say_name/:name', to: 'site#say_name'
+  get '/calculator', to: 'site#calculator'
+  post '/calculate', to: 'site#calculate'
 end
