9f48e80d94f9b110e52a78d37bcf12d318301615
lorenzo892 <lorenzo.gimeno.ruiz@gmail.com>
2016-07-05 12:24:34 +0200

    add values to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create(name: "crazy project", description: "what an amazing project")
+Project.create(name: "Fast project", description: "what an amazing fast project")
+Project.create(name: "Lazzy project", description: "what an amazing lazzy project")
+Project.create(name: "poor project", description: "what an amazing poor project")
+Project.create(name: "the project", description: "what an amazing project")
+Project.create(name: "big project", description: "what an amazing big project")
+Project.create(name: "smart project", description: "what an amazing smart project")
+Project.create(name: "quick project", description: "what an amazing quick project")
+Project.create(name: "polite project", description: "what an amazing politeproject")
+Project.create(name: "serious project", description: "what an amazing serious project")
+Project.create(name: "tea project", description: "what an amazing tea project")
+Project.create(name: "coffee project", description: "what an amazing coffee project")
