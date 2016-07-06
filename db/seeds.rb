ae968915b33d669dfba09b2d845059a8533e7462
lorenzo892 <lorenzo.gimeno.ruiz@gmail.com>
2016-07-06 12:49:12 +0200

    create new time entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Project.create(name: "crazy project", description: "what an amazing project")
+Project.create(name: "crazy project", description: "what an amazing project")4
+
 Project.create(name: "Fast project", description: "what an amazing fast project")
 Project.create(name: "Lazzy project", description: "what an amazing lazzy project")
 Project.create(name: "poor project", description: "what an amazing poor project")
@@ -18,3 +19,17 @@ Project.create(name: "polite project", description: "what an amazing politeproje
 Project.create(name: "serious project", description: "what an amazing serious project")
 Project.create(name: "tea project", description: "what an amazing tea project")
 Project.create(name: "coffee project", description: "what an amazing coffee project")
+
+TimeEntry.create(project_id: 2, hours: 10, minutes: 30, comments: "commenting the time entry")
+TimeEntry.create(project_id: 4, hours: 30, minutes: 20, comments: "commenting  other the time entry")
+
+# another way
+
+another_project = Project.create(name: "coffee project", description: "what an amazing coffee project")
+TimeEntry.create(hours: 6).project = another_project
+TimeEntry.create(hours:4, project_id:2)
+
+# another way
+
+project = Project.create(name:"Time tracking app")
+project.time_entries.create(hours:6)
