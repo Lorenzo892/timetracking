6c830da70d34b0d10e9756089b91d1a3dc6fc7c0
lorenzo892 <lorenzo.gimeno.ruiz@gmail.com>
2016-07-06 10:22:48 +0200

    create new projects via form

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get '/calculator', to: 'site#calculator'
   post '/calculate', to: 'site#calculate'
   get '/projects', to: 'projects#index'
+  get '/projects/new', to: 'projects#new'
   get 'projects/:id', to: 'projects#show'
+
+  post '/projects', to: 'projects#create'
 end
